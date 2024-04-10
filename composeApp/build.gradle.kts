96e7c5b48699333fefbf7a3863e12ffc44e2fba1
vladbalaneanu <vlad.bala17@gmail.com>
2024-04-10 10:27:59 +0300

    added kotlin datetime dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,6 +39,10 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            implementation(libs.kotlinx.datetime)
+
+            
         }
     }
 }
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -14,6 +14,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import bikeappkmp.composeapp.generated.resources.Res
 import bikeappkmp.composeapp.generated.resources.compose_multiplatform
+import kotlinx.datetime.Clock
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
