b6ca8c893aa57debae7af1bd13549c2206df94c0
vladbalaneanu <vlad.bala17@gmail.com>
2024-04-24 15:03:28 +0300

    fixed empty header text

--- composeApp/src/commonMain/kotlin/bikes/presentation/list/components/EmptyHeader.kt
+++ composeApp/src/commonMain/kotlin/bikes/presentation/list/components/EmptyHeader.kt
@@ -2,17 +2,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
 import bikeappkmp.composeapp.generated.resources.Res
 import bikeappkmp.composeapp.generated.resources.add_bike_label
 import bikeappkmp.composeapp.generated.resources.add_ride_label
-import bikeappkmp.composeapp.generated.resources.dotted_line
 import bikeappkmp.composeapp.generated.resources.missing_bike_card
 import bikeappkmp.composeapp.generated.resources.no_bikes_info
 import bikes.presentation.list.components.ActionButton
@@ -42,16 +39,10 @@ fun EmptyHeader(
                 .align(Alignment.CenterHorizontally)
                 .weight(1f)
         ) {
-            Image(
-                painter = painterResource(Res.drawable.dotted_line),
-                contentDescription = "dotted line",
-                modifier = Modifier
-                    .padding(start = 20.dp).heightIn(min = 150.dp, max = 150.dp)
-
-            )
             if (showText) {
                 Text(
                     text = stringResource(Res.string.no_bikes_info),
+                    textAlign = TextAlign.Center,
                     modifier = Modifier
                         .align(Alignment.Center)
                 )
