4120d46a822dcdad0147df67e80e6a701d8d5c8e
vladbalaneanu <vlad.bala17@gmail.com>
2024-05-09 15:26:29 +0300

    injected view models

--- composeApp/src/androidMain/kotlin/di/PlatformModule.kt
+++ composeApp/src/androidMain/kotlin/di/PlatformModule.kt
@@ -1,8 +1,15 @@
 package di
 
+import bikes.presentation.addbike.AddBikeViewModel
+import bikes.presentation.detail.BikeDetailViewModel
+import bikes.presentation.list.BikesViewModel
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.vlad.kmp.database.BikesDatabase
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import rides.presentation.addride.AddRideViewModel
+import rides.presentation.detail.RideDetailViewModel
+import rides.presentation.list.RidesViewModel
 
 actual class PlatformModule {
     actual val modules = module {
@@ -14,5 +21,13 @@ actual class PlatformModule {
             )
         }
         single { BikesDatabase(get()) }
+
+        viewModel { AddBikeViewModel(get(), get(), get()) }
+        viewModel { BikeDetailViewModel(get(), get()) }
+        viewModel { BikesViewModel(get(), get()) }
+
+        viewModel { AddRideViewModel(get(), get(), get()) }
+        viewModel { RideDetailViewModel() }
+        viewModel { RidesViewModel(get()) }
     }
 }
--- composeApp/src/iosMain/kotlin/di/PlatformModule.kt
+++ composeApp/src/iosMain/kotlin/di/PlatformModule.kt
@@ -1,12 +1,26 @@
 package di
 
+import bikes.presentation.addbike.AddBikeViewModel
+import bikes.presentation.detail.BikeDetailViewModel
+import bikes.presentation.list.BikesViewModel
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 import com.vlad.kmp.database.BikesDatabase
 import org.koin.dsl.module
+import rides.presentation.addride.AddRideViewModel
+import rides.presentation.detail.RideDetailViewModel
+import rides.presentation.list.RidesViewModel
 
 actual class PlatformModule {
     actual val modules = module {
-        single { NativeSqliteDriver(BikesDatabase.Schema,"bikes.db") }
+        single { NativeSqliteDriver(BikesDatabase.Schema, "bikes.db") }
         single { BikesDatabase(get()) }
+
+        factory { AddBikeViewModel(get(), get(), get()) }
+        factory { BikeDetailViewModel(get(), get()) }
+        factory { BikesViewModel(get(), get()) }
+
+        factory { AddRideViewModel(get(), get(), get()) }
+        factory { RideDetailViewModel() }
+        factory { RidesViewModel(get()) }
     }
 }
